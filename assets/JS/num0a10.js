11285c5e3ade6ceaedaced9b0dfa3424401df5f5
CristianS <cristianserrano79@gmail.com>
2024-05-13 12:20:02 -0300

    se termino el ejercicio

---
+++ assets/JS/num0a10.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 11; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " Es par")
+    } else {
+        console.log(i + " Es impar")
+    }
+}
---
+++ assets/JS/triangulo.js
@@ -0,0 +1,4 @@
+let a = ""
+for (let i = 1; i <= 5; i++) {
+    console.log(a += "#")
+}
